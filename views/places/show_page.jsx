8770b50f07268be66d7cc92319e71a552249d1d2
Jeremy Nelson <jeremy.nelson8@gmail.com>
2022-02-18 21:04:03 -0500

    individual place/restaurant views added by index

--- views/places/show_page.jsx
+++ views/places/show_page.jsx
@@ -1,12 +1,40 @@
 const React = require('react')
 const Def = require('../default')
 
-function show_page () {
+function show_page (data) {
     //this function will return details for a single page
     return (
         <Def>
             <main>
-                <h1>Show Page</h1>
+                <div className='row'>
+                    <div className='col-sm-6'>
+                        <img src={data.place.pic} alt={data.place.name} />
+                    </div>
+                    <div className='col-sm-6'>
+                        <h1>{data.place.name}</h1>
+                        <h2>
+                            User Rating:                            
+                        </h2>
+                        <h3>
+                            Not Rated. Add A Rant!
+                        </h3>
+                        <br />
+                        <h2>
+                            RESTaurant Bio:
+                        </h2>
+                        <h3>
+                            Located in {data.place.city}, {data.place.state}
+                        </h3>
+                        <h3>
+                            Enjoy their {data.place.cuisines}
+                        </h3>
+                    </div>
+                </div>
+                <hr />
+                <h2>Rants and Comments!</h2>
+                <h3>
+                    No Rants yet! Tell us how you really feel!
+                </h3>
             </main>
         </Def>
     )
